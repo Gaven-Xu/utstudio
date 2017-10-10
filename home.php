7c3abb4b0c41668224d0f2d3bbe5d28cd59457e1
Demon <zq1994zb@qq.com>
2017-10-10 14:50:30 +0800

    banner

--- home.php
+++ home.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <?php include('parts/nav.php')?>
+<?php include('parts/banner.php')?>
 
     <?php query_posts(array('category_name'=>'pic','posts_per_page'=>5));if(have_posts()):while(have_posts()):the_post();?>
         <?php if(get_content_first_image(get_the_content())):?>
