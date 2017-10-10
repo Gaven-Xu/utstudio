9121228f08efd8b6d0f8333d3e40cd25c48f35a7
Gaven <xu@unnuo.com>
2017-10-10 10:36:37 +0800

    add banner part files

--- parts/nav.php
+++ parts/nav.php
@@ -1,3 +1,10 @@
 </head>
 <body>
-    <?php wp_nav_menu()?>
+    <div id="Nav" class="clear">
+        <div class="logo">
+            <div class="logo_main"></div>
+            <div class="cut_off"></div>
+            <div class="logo_side"></div>
+        </div>
+        <?php wp_nav_menu()?>
+    </div>
