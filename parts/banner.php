2d5a9f8513c9d5612b45e845e4ed151984ab973f
Demon <zq1994zb@qq.com>
2017-10-10 15:29:36 +0800

    修改banner样式表

--- parts/banner.php
+++ parts/banner.php
@@ -1,28 +1,29 @@
-
-<div class="main_visual">
-    <div class="flicking_con">
-        <div class="flicking_inner">
-            <a href="javascript:" class=""></a>
-            <a href="javascript:" class=""></a>
-            <a href="javascript:" class="on"></a>
+<div id="banner">
+    <div class="main_visual">
+        <div class="flicking_con">
+            <div class="flicking_inner">
+                <a href="javascript:" class=""></a>
+                <a href="javascript:" class=""></a>
+                <a href="javascript:" class="on"></a>
+            </div>
+        </div>
+        <div class="main_image">
+            <ul>
+                <li class="li_1">
+                    <a class="img_1" href="javascript:" target="_blank"></a>
+                </li>
+                <li>
+                    <a class="img_2" href="javascript:" target="_blank"></a>
+                </li>
+                <li>
+                    <a class="img_3" href="javascript:" target="_blank"></a>
+                </li>
+            </ul>
+            <a href="javascript:" id="btn_prev"></a>
+            <a href="javascript:" id="btn_next"></a>
         </div>
     </div>
-    <div class="main_image">
-        <ul>
-            <li class="li_1">
-                <a class="img_1" href="javascript:" target="_blank"></a>
-            </li>
-            <li>
-                <a class="img_2" href="javascript:" target="_blank"></a>
-            </li>
-            <li>
-                <a class="img_3" href="javascript:" target="_blank"></a>
-            </li>
-        </ul>
-        <a href="javascript:" id="btn_prev"></a>
-        <a href="javascript:" id="btn_next"></a>
-    </div>
 </div>
-<script src="<?php bloginfo('template_url')?>/assets/jquery-3.2.1.js"></script>
-<script src="<?php bloginfo('template_url')?>/assets/jquery.touchSlider.js"></script>
-<script src="<?php bloginfo('template_url')?>/script/banner.js"></script>
+<script src="<?php bloginfo('template_url') ?>/assets/jquery-3.2.1.js"></script>
+<script src="<?php bloginfo('template_url') ?>/assets/jquery.touchSlider.js"></script>
+<script src="<?php bloginfo('template_url') ?>/script/banner.js"></script>
