584a7b793bdcdcf5c8b0d1a46e8cde2c850bd48e
Gaven <xu@unnuo.com>
2017-10-26 17:25:44 +0800

    del died code in nav and banner

--- parts/nav.php
+++ parts/nav.php
@@ -2,9 +2,6 @@
 <body>
     <div id="Nav" class="clear">
         <div class="logo">
-            <div class="logo_main"></div>
-            <div class="cut_off"></div>
-            <div class="logo_side"></div>
         </div>
         <?php wp_nav_menu()?>
     </div>
